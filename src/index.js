b16bba34d37cd481aaa16f9a86679276a5744648
Varunreddy1328 <kesireddyvarunreddy08@gmail.com>
2026-06-21 07:18:16 +0530

    added sechighest_array code

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ function reverseWord(str) {
     }
     return res;
 }
+//2. Highest of Array.....
 function highestNumberInArray(arr) {
     var high = 0;
     for (var i = 0; i <= arr.length - 1; i++) {
@@ -15,5 +16,20 @@ function highestNumberInArray(arr) {
     }
     return high;
 }
-console.log(reverseWord("array"));
-console.log(highestNumberInArray([1, 2, 6, 5, 3]));
+// console.log(reverseWord("array"));
+// console.log(highestNumberInArray([1,2,6,5,3]))
+console.log(secondHighArray([1, 2, 6, 5, 3]));
+//3. second highest of Array...
+function secondHighArray(arr) {
+    var sec_high = -Infinity, high = -Infinity;
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] > high) {
+            sec_high = high;
+            high = arr[i];
+        }
+        else if (arr[i] > sec_high && sec_high !== high) {
+            sec_high = arr[i];
+        }
+    }
+    return sec_high;
+}
--- src/index.ts
+++ src/index.ts
@@ -24,5 +24,24 @@ function highestNumberInArray(arr:number[]):number{
     return high;
 }
 
-console.log(reverseWord("array"));
-console.log(highestNumberInArray([1,2,6,5,3]))
+// console.log(reverseWord("array"));
+// console.log(highestNumberInArray([1,2,6,5,3]))
+console.log(secondHighArray([1,2,6,5,3]))
+
+//3. second highest of Array...
+function secondHighArray(arr:number[]):number{
+
+    let sec_high=-Infinity,high=-Infinity;
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]>high){
+            sec_high = high;
+            high = arr[i];
+        }else if(arr[i]>sec_high && sec_high!==high){
+            sec_high = arr[i];
+        }
+
+    }
+
+    return sec_high;
+    
+}
